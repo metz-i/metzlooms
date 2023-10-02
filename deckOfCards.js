5bc8f279740a54e123053748b1a0cf92d5c43eae
Metz I <128658219+metz-i@users.noreply.github.com>
2023-10-02 15:29:12 -0400

    deck of cards: destructuring,  keyword, and methods

--- deckOfCards.js
+++ deckOfCards.js
@@ -1,17 +1,19 @@
 const myDeck = {
-    deck: [],
-    suits: ['hearts', 'diamonds', 'clubs', 'spades'],
-    values: 'A,2,3,4,5,6,7,8,9,10,J,Q,K',
+    deck : [],
+    suits : ['hearts', 'diamonds','spades', 'clubs'],
+    values : 'A,2,3,4,5,6,7,8,9,10,J,Q,K',
+
     initializeDeck() {
-        const { 
+        const {
+            deck, 
             suits, 
-            values, 
-            deck } = this;
-        for (let value of values.split(',')) {
-            for (let suit of suits) {
+            values
+        } = this;
+        for (let suit of suits){
+            for (let value of values.split(',')) {
                 deck.push({
                     value,
-                    suit
+                    suit,
                 })
             }
         }
@@ -20,6 +22,3 @@ const myDeck = {
         return this.deck.pop()
     }
 }
-
-const deck1 = makeDeck();
-const card1 = drawCard(deck1);
